c275247c7d14846f4cf87ce4af25800058e1018a
Yana Kozhevnikova <y.kozhev@yandex.ru>
2020-09-14 19:18:11 +0700

    Updates

---
+++ precache-manifest.499bc7ad710139f939c76803c1a22b03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2d57a92c877711458076a67c342039",
+    "url": "/cryptography-labs/index.html"
+  },
+  {
+    "revision": "94c5efd3ce0aa44acef1",
+    "url": "/cryptography-labs/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f21cef63cb4f930735c8",
+    "url": "/cryptography-labs/static/css/main.1ce3f0b2.chunk.css"
+  },
+  {
+    "revision": "94c5efd3ce0aa44acef1",
+    "url": "/cryptography-labs/static/js/2.fea99d46.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cryptography-labs/static/js/2.fea99d46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f21cef63cb4f930735c8",
+    "url": "/cryptography-labs/static/js/main.79f07ac7.chunk.js"
+  },
+  {
+    "revision": "5ba6a05f0429f9f1a274",
+    "url": "/cryptography-labs/static/js/runtime-main.9a3bb5fd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cryptography-labs/precache-manifest.fd677694ef12495ab42031cdcc4e4a1d.js"
+  "/cryptography-labs/precache-manifest.499bc7ad710139f939c76803c1a22b03.js"
 );
 
 self.addEventListener('message', (event) => {
